0a5520cccccb5257795d875e2f74356692cf0fdd
Amaya <98649425+AkameTheCoder@users.noreply.github.com>
2023-10-05 06:19:48 -0700

    Update and rename deleteBlocks.rs to delete_blocks.rs
    changing function name to only lowercase

--- rust_core/src/delete_blocks.rs
+++ rust_core/src/delete_blocks.rs
@@ -4,7 +4,7 @@ use std::io::ErrorKind;
 use std::io::Result;
 use std::fs::OpenOptions;
 
-pub fn deleteBlocks(input: String) {
+pub fn delete_Blocks(input: String) {
      
     let enddate = input;
     
