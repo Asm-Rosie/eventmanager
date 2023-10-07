177c62c5a7ff5c3460ab27932e8ba512c91a3e5f
Amaya <98649425+AkameTheCoder@users.noreply.github.com>
2023-10-07 05:56:36 -0700

    adding a new method for building

---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+echo "checking your OS.."
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "OS is MacOS, running build script!"
+
+elif [["$OSTYPE" == "linux-gnu"* ]]; then
+  echo "OS is Linux, and Linux is currently not supported"
+  exit 1
+
+else
+  echo "OS is not supported"
+  exit 1
+fi
+
+if command -v cargo >/dev/null 2>&1; then
+  echo "cargo is installed"
+else
+  echo "cargo is not installed"
+  echo "this script can install cargo and all needed resources automatically for you, if you prefer to install cargo manually, go to https://www.rust-lang.org/tools/install and follow the instructions"
+  echo "do you want that I install cargo for you?"
+  echo "y/n"
+  read answer
+  if [ "$answer" == "y" ]; then
+    command curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    echo "installed cargo!"
+    echo "installing necessary resources.."
+    command rustup target add x86_64-darwin
+    command rustup target add aarch64-apple-darwin
+    echo "installed resources!"
+  elif [ "$answer" == "n" ]; then
+    echo "exiting.."
+    exit 1
+  fi
+  
+fi
+
+X64_APPLE_INPUT_FOLDER="rust_core/target/x86_64-apple-darwin/release/"
+ARM_APPLE_INPUT_FOLDER="rust_core/target/aarch64-apple-darwin/release/"
+DESTINATION_FOLDER="eventmanager/rust_core/"
+
+echo "do you want to start the building process?"
+echo "y/n"
+read answer
+if [ "$answer" == "y" ]; then
+  echo "building.."
+  command rustup target add x86_64-apple-darwin
+  command rustup target add aarch64-apple-darwin
+  echo "targets have been installed or are up to date"
+  command cargo build --release --target x86_64-apple-darwin --manifest-path ./rust_core/Cargo.toml
+  command cargo build --release --target aarch64-apple-darwin --manifest-path ./rust_core/Cargo.toml
+  echo "building finished!"
+  echo "checking if there is already an existing build.."
+  if [ -f "eventmanager/rust_core/binded_lib.a" ]; then
+    echo "there is already an existing build. creating backup.. *just in case*"
+    mkdir "$DESTINATION_FOLDER/backups"
+    mv "$DESTINATION_FOLDER/binded_lib.a" "$DESTINATION_FOLDER/backups/binded_lib.a"
+    echo "backup created"
+  fi
+  echo "binding libraries.."
+  command lipo -create -output $DESTINATION_FOLDER/binded_lib.a $X64_APPLE_INPUT_FOLDER/libeventmanager_core.a $ARM_APPLE_INPUT_FOLDER/libeventmanager_core.a
+  echo "binded libraries successfully successfully"
+  command lipo -info $DESTINATION_FOLDER/binded_lib.a
+  echo "building finished successfully!, you can now build your app in Xcode by just hitting the run button"
+fi
