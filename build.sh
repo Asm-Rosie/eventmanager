0e019befa72c8c78125aaca0144f56a15420e58f
Amaya <98649425+AkameTheCoder@users.noreply.github.com>
2023-10-16 23:23:25 +0200

    changing some things for the dynamic lib

--- build.sh
+++ build.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-
-# Set terminal colors
-GREEN=$(tput setaf 2)   # Green text
-RED=$(tput setaf 1)     # Red text
-RESET=$(tput sgr0)      # Reset text color
-
-BOLD="\033[1m"
-RESETANSI="\033[0m"
-
-echo "${GREEN}checking your OS..${RESET}"
+echo "checking your OS.."
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  echo -e "OS is ${BOLD}MacOS${RESETANSI}, running build script!"
+  echo "OS is MacOS, running build script!"
 
 elif [["$OSTYPE" == "linux-gnu"* ]]; then
   echo "OS is Linux, and Linux is currently not supported"
@@ -28,6 +19,9 @@ DESTINATION_FOLDER="eventmanager/rust_core/"
 
 
 echo "building.."
+command rustup target add x86_64-apple-darwin
+command rustup target add aarch64-apple-darwin
+echo "targets have been installed or are up to date"
 command cargo build --release --target x86_64-apple-darwin --manifest-path ./rust_core/Cargo.toml
 command cargo build --release --target aarch64-apple-darwin --manifest-path ./rust_core/Cargo.toml
 echo "building finished!"
@@ -36,11 +30,10 @@ if [ -f "eventmanager/rust_core/binded_lib.a" ]; then
   echo "there is already an existing build. creating backup.. *just in case*"
   mkdir "$DESTINATION_FOLDER/backups"
   mv "$DESTINATION_FOLDER/binded_lib.a" "$DESTINATION_FOLDER/backups/binded_lib.a"
-  echo "${GREEN}backup created${RESET}"
+  echo "backup created"
 fi
 echo "binding libraries.."
-command lipo -create -output $DESTINATION_FOLDER/binded_lib.a $X64_APPLE_INPUT_FOLDER/libeventmanager_core.a $ARM_APPLE_INPUT_FOLDER/libeventmanager_core.a
-echo -e "${BOLD}binded libraries successfully successfully${RESETANSI}"
+command lipo -create -output $DESTINATION_FOLDER/binded_lib.a $X64_APPLE_INPUT_FOLDER/libeventmanager_core.dylib $ARM_APPLE_INPUT_FOLDER/libeventmanager_core.dylib
+echo "binded libraries successfully successfully"
 command lipo -info $DESTINATION_FOLDER/binded_lib.a
-echo "${GREEN}operation complete!${RESET}"
-echo "you can now build your app in Xcode by just hitting the run button"
+echo "building finished successfully!, you can now build your app in Xcode by just hitting the run button"
